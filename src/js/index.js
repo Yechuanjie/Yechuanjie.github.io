2d1ba4a3938443ca8d5f5130aeaa441eecb105af
yechuanjie <1102684711@qq.com>
2019-12-02 14:38:39 +0800

    更新首页

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
+/* eslint-disable max-len */
+/* eslint-disable no-mixed-operators */
 import '../scss/index.scss';
 
 // const GistID = 'a5b546db3729a3408815b3793dd7c777';
 // console.log('%c Setting Sync ', 'background: #234;color:#bada55;font-size:25px');
 // console.warn('1. install Setting Sync \n 2. press shift + alt + d \n 3. input GistID:' + GistID);
 $(function() {
+	initRelIcon();
+	initWrapBack();
 	console.log(
 		'%c琥珀丶川',
 		`text-shadow:
@@ -26,3 +30,59 @@ $(function() {
 		background:url('http://yechuanjie-image.oss-cn-beijing.aliyuncs.com/18-5-31/66337986.jpg') no-repeat;
 		background-size:contain;`);
 });
+
+function initRelIcon() {
+	let OriginTitle = document.title || '琥珀丶川';
+	let titleTime;
+	let iconList = [
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/腰果.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/甜甜圈.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/薯片.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/塔可.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/披萨.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/麻薯.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/热狗.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/寿司.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/牛油果.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/蛋黄酥.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/面包.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/马卡龙.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/饼干.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/草莓.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/蛋糕.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/开心果.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/布丁.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/爆米花.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/碧根果.png',
+		'https://yechuanjie-image.oss-cn-beijing.aliyuncs.com/blog/棒冰.png'
+	];
+	let iconIndex = 0;
+	$(document).on('visibilitychange', function() {
+		console.info(`hidden`);
+		if (document.hidden) {
+			if (iconIndex < iconList.length - 1) {
+				iconIndex += 1;
+			}
+			else {
+				iconIndex = 0;
+			}
+			$('[rel=icon]').attr('href', iconList[iconIndex]);
+			document.title = OriginTitle;
+			clearTimeout(titleTime);
+		}
+		else {
+			document.title = OriginTitle;
+			$('[rel=icon]').attr('href', iconList[iconIndex]);
+			titleTime = setTimeout(function() {
+				document.title = OriginTitle;
+			}, 2000);
+		}
+	});
+}
+
+function initWrapBack() {
+	const i = Math.floor(Math.random());
+	$('#wrapper').css('background', `linear-gradient(to left bottom,
+	hsl(${(Math.floor(255 * Math.random()) + i)}, 40%, 50%),
+	hsl(${(Math.floor(255 * Math.random()) + i)}, 40%, 50%)`);
+}
